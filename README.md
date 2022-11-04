aec7b6b604b709a4c548020eb5eeb7018a72dbd6
fbgabriana <fbgabriana@gmail.com>
2022-11-04 12:35:04 +0800

    Test Contact Us

---
+++ README.md
@@ -0,0 +1,16 @@
+# Mini Project 2 - Cyber Homies Group Website
+
+This is a group miniproject for the [KodeGo Web Developer Course](https://kodego.ph/courses/1) I am taking.
+
+**_Update_: Template has been completed.**
+
+* [View the template](template.html)
+
+**_Update_: Created dummy pages.**
+
+* [View the homepage](home.html)
+
+* Control files:
+
+  - [site.webmanifest](site.webmanifest)
+  - [site.webicon.svg](site.webicon.svg)
---
+++ scripts/scripts.js
@@ -0,0 +1,82 @@
+let setBackground = () => {
+
+	const d0 = new Date();
+	const getFrameNumber = () => {
+		let d1 = new Date();
+		return Math.floor( ( d1.getTime() - d0.getTime() ) / 4000 ) % 6;
+	}
+
+	const getColorScheme = () => {
+		return window.matchMedia("(prefers-color-scheme: dark)").matches;
+	}
+
+	const benefits = ["marketability online", "growth opportunities", "competitiveness", "market expansion", "consumer services", "your credibility"];
+
+	const preloadImages = () => {
+		let img1 = []; let img2 = [];
+		for (let i = 0; i < 6; i++) {
+			img1[i] = new Image(); img2[i] = new Image();
+			img1[i].src = `images/background${i}.jpg`;
+			img2[i].src = `images/background${i}-dark.jpg`;
+		}
+	}
+	preloadImages();
+
+	const changeFrame = (b) => {
+		f = getFrameNumber(); p = (f + 5) % 6;
+		s = getColorScheme() ? "-dark" : "";
+		if (location.href.includes("home.html") || location.href.includes("template.html")) {
+			if (b) {
+				document.body.style.backgroundImage = `url(images/background0${s}.jpg), url(images/background${s}.jpg)`;
+			} else {
+				document.body.style.backgroundImage = `url(images/background${f}${s}.jpg), url(images/background${p}${s}.jpg)`;
+			}
+			let boost = document.querySelector(".boost");
+			if (boost) boost.childNodes[0].nodeValue = benefits[f];
+		}
+		if (location.href.includes("about.html")) {
+			document.body.style.backgroundImage = `url(images/background1${s}.jpg), url(images/background${s}.jpg)`;
+		}
+		if (location.href.includes("projects.html")) {
+			document.body.style.backgroundImage = `url(images/background3${s}.jpg), url(images/background${s}.jpg)`;
+		}
+		if (location.href.includes("contact.html")) {
+			document.body.style.backgroundImage = `url(images/background4${s}.jpg), url(images/background${s}.jpg)`;
+		}
+	}
+	changeFrame(true);
+
+	setInterval(rotateFrames = () => {
+		changeFrame();
+	}, 4000);
+
+	let currentColorScheme = "";
+	setInterval(checkSchemeChange = () => {
+		checkColorScheme = getColorScheme();
+		if (currentColorScheme != checkColorScheme) {
+			currentColorScheme = checkColorScheme;
+			changeFrame();
+		}
+	}, 100);
+}
+
+let setCurrentPage = () => {
+	var a = document.querySelectorAll("a");
+	var currentLocation = location.href.replace("index.html","");
+	for (i=0; i<a.length; i++) {
+		if (a[i].href == currentLocation) {
+			a[i].classList.add("current-page");
+			a[i].removeAttribute("href");
+			a[i].removeAttribute("title");
+		}
+		if (currentLocation.includes(a[i].href)) {
+			a[i].classList.add("current-section");
+		}
+	}
+}
+
+window.addEventListener("load", event => {
+	setCurrentPage();
+	setBackground();
+});
+
